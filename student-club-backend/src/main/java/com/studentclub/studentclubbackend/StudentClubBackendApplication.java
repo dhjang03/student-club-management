2809c8f42ee87525e424dafbef30789a3dbe3cf0
Dong Hyeog Jang <141971905+dhjang03@users.noreply.github.com>
2025-01-08 19:27:21 +1100

    Initial Commit

---
+++ student-club-backend/src/main/java/com/studentclub/studentclubbackend/StudentClubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.studentclub.studentclubbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentClubBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentClubBackendApplication.class, args);
+    }
+
+}
---
+++ student-club-backend/src/test/java/com/studentclub/studentclubbackend/StudentClubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentclub.studentclubbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentClubBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
