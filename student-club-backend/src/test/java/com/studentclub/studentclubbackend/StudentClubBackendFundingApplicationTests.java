33b4d85d169ddf853deec353d3feddabdd531762
Dong Hyeog Jang <141971905+dhjang03@users.noreply.github.com>
2025-01-10 16:07:11 +1100

    feat: Configure PostgreSQL datasource and update test class name

--- student-club-backend/src/test/java/com/studentclub/studentclubbackend/StudentClubBackendFundingApplicationTests.java
+++ student-club-backend/src/test/java/com/studentclub/studentclubbackend/StudentClubBackendFundingApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class StudentClubBackendApplicationTests {
+class StudentClubBackendFundingApplicationTests {
 
     @Test
     void contextLoads() {
