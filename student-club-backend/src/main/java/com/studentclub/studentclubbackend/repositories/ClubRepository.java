0be5b2af5437a2eeff8eb012723ba16ac678a786
Dong Hyeog Jang <141971905+dhjang03@users.noreply.github.com>
2025-01-15 10:41:21 +1100

    refactor: remove unused import and method from ClubRepository

--- student-club-backend/src/main/java/com/studentclub/studentclubbackend/repositories/ClubRepository.java
+++ student-club-backend/src/main/java/com/studentclub/studentclubbackend/repositories/ClubRepository.java
@@ -1,11 +1,7 @@
 package com.studentclub.studentclubbackend.repositories;
 
 import com.studentclub.studentclubbackend.models.Club;
-import com.studentclub.studentclubbackend.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface ClubRepository extends JpaRepository<Club, Long> {
-    List<Club> findAllByAdminsContaining(User admin);
 }
