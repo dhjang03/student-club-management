e806168acf708d0477f5c8468bd612e0c5b7e57b
Dong Hyeog Jang <141971905+dhjang03@users.noreply.github.com>
2025-01-16 22:13:30 +1100

    chore: simplify HTML structure in RootLayout component

--- student-club-frontend/src/app/layout.tsx
+++ student-club-frontend/src/app/layout.tsx
@@ -14,9 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html 
-      lang="en"
-    >
+    <html lang="en">
       <body>
         {children}
       </body>
