0ba18dba5c95b484d06f1b686422d1406f14c212
Dong Hyeog Jang <141971905+dhjang03@users.noreply.github.com>
2025-01-17 21:54:39 +1100

    feat: add types for DTOs

--- student-club-frontend/src/model/domains.tsx
+++
@@ -1,9 +0,0 @@
-export type ClubEvent = {
-    id: string
-    title: string
-    description: string
-    date: Date
-    venue: string
-    capacity: number
-    attendees: number
-}
---
+++ student-club-frontend/src/types/dashboard.ts
@@ -0,0 +1,93 @@
+export enum UserRole {
+    STUDENT = 'STUDENT',
+    STAFF = 'STAFF'
+}
+
+
+export enum ApplicationStatus {
+    DRAFT = "DRAFT",
+    SUBMITTED = "SUBMITTED",
+    IN_REVIEW = "IN_REVIEW",
+    APPROVED = "APPROVED",
+    REJECTED = "REJECTED"
+}
+
+
+export interface ClubDTO {
+    id: number;
+    name: string;
+    description: string;
+    funds: number;
+}
+
+export interface ClubMemberDTO {
+    id: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+    membership: string;
+}
+
+export interface VenueDTO {
+    id: number;
+    capacity: number;
+    name: string;
+    address: string;
+}
+
+export interface EventDTO {
+    id: number;
+    title: string;
+    description: string;
+    date: string;
+    venue: VenueDTO;
+    cost: number;
+    createdAt: string;
+    capacity: number;
+    numOfAttendees: number;
+}
+
+export interface FundingDTO {
+    id: number;
+    amount: number;
+    description: string;
+    createdAt: string;
+    status: ApplicationStatus;
+}
+
+export interface LoginRequestDTO {
+    username: string;
+    password: string;
+}
+
+export interface RegisterRequestDTO {
+    username: string;
+    password: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    roles: UserRole[];
+}
+
+export interface TicketDTO {
+    id: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+}
+
+export interface UserDTO {
+    id: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+    roles: UserRole[];
+}
+
+export interface RsvpDTO {
+    id: string;
+    responder: UserDTO;
+    event: EventDTO;
+    tickets: TicketDTO[];
+    createdAt: string;
+}
