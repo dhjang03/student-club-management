34a7427e83cf0194b689c48829c60fa476889bcb
Dong Hyeog Jang <141971905+dhjang03@users.noreply.github.com>
2025-01-18 21:19:06 +1100

    fix: update CORS configuration to use allowedOriginPatterns for better flexibility

--- student-club-backend/src/main/java/com/studentclub/studentclubbackend/security/WebConfig.java
+++ student-club-backend/src/main/java/com/studentclub/studentclubbackend/security/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(frontEndUrl)
+                .allowedOriginPatterns(frontEndUrl)
                 .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
