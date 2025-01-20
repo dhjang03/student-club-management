dd8df355432691c81f56d7ae79ba8aa6ab32334a
Dong Hyeog Jang <141971905+dhjang03@users.noreply.github.com>
2025-01-20 15:49:14 +1100

    fix: remove redirection to dashboard home on refresh

--- student-club-frontend/src/hocs/ProtectedRoutes.tsx
+++ student-club-frontend/src/hocs/ProtectedRoutes.tsx
@@ -3,8 +3,9 @@
 import React, { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { useAuth } from '@/context/AuthContext';
-import { getUserRole, isTokenValid, getUserId } from '@/utils/auth';
+import { getUserRole, isTokenValid } from '@/utils/auth';
 import { UserRole } from '@/types/dashboard';
+import { clearToken } from '@/utils/auth';
 
 interface ProtectedRouteProps {
   children: React.ReactNode;
@@ -14,20 +15,22 @@ export default function ProtectedRoute({ children }: ProtectedRouteProps) {
   const router = useRouter();
   const { token } = useAuth();
 
+  // useEffect(() => {
+  //   if (token && isTokenValid(token)) {
+  //     const roles = getUserRole(token);
+  //     if (roles.includes(UserRole.STUDENT)) {
+  //       router.push('/student-dashboard');
+  //     } else if (roles.includes(UserRole.STAFF)) {
+  //       router.push('/staff-dashboard');
+  //     }
+  //   } else {
+  //     router.push('/login');
+  //   }
+  // }, [token, router]);
+
   useEffect(() => {
-    if (token && isTokenValid(token)) {
-      const roles = getUserRole(token);
-
-      const userId = getUserId(token);
-      console.log(userId);
-      console.log(roles);
-
-      if (roles.includes(UserRole.STUDENT)) {
-        router.push('/student-dashboard');
-      } else if (roles.includes(UserRole.STAFF)) {
-        router.push('/staff-dashboard');
-      }
-    } else {
+    if (!token || !isTokenValid(token)) {
+      clearToken();
       router.push('/login');
     }
   }, [token, router]);
